6eb4686f16bae5c555dc456d0ad213f5afab5936
tahirivazquez <tahiwdev@gmail.com>
2023-09-03 16:01:57 -0300

    Probando soluciones

--- js/script.js
+++ js/script.js
@@ -39,4 +39,10 @@ function showList(array) {
 document.addEventListener("DOMContentLoaded", (e) => {
   // Escribe tu solución aquí
   // Sugerencia de cómo mostrar el array => showList(strangeArray);
-});
+    const stringElements = strangeArray.filter((element) => typeof element === "string");
+
+    stringElements.sort();
+  
+    showList(stringElements);
+  });
+
